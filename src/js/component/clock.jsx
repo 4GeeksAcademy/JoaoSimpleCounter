5824341d8bfa930575753a9a375d6ff8d23ec4d9
joaojcfr <joao.ferreira95@yahoo.com.br>
2024-06-15 18:41:52 +0000

    Final Version

--- src/js/component/clock.jsx
+++ src/js/component/clock.jsx
@@ -1,30 +1,47 @@
 import React, { useState, useEffect } from 'react';
 
- function Clock() {
-    const [count, setCount] = useState(0);
+function Clock() {
+  const [count, setCount] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-          setCount((prevCount) => prevCount + 1);
-        }, 1000);
-    
-        // Cleanup function to clear the interval when the component unmounts
-        return () => clearInterval(intervalId);
-      }, []); // Empty dependency array ensures this effect runs once on mount and not on updates
-    
-      return (
-        <div className="mainContainer d-flex justify-content-center">
-            <div className="clock icon">
-                0         
-            </div>
-            <div className="clock number0">0</div>
-            <div className="clock number1">0</div>
-            <div className="clock number2">0</div>
-            <div className="clock number3">0</div>
-            <div className="clock number4">0</div>
-            <div className="clock number5">{count}</div>
+  useEffect(() => {
+    let intervalId;
+
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setCount((prevCount) => prevCount + 1);
+      }, 100);
+    }
+
+    // Cleanup function to clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, [isRunning]);
+
+  const handleStart = () => {
+    setIsRunning(isRunning ? false : true)
+    //setIsRunning(true);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+    setIsRunning(false);
+  };
+
+  // Format the count to display six digits (HH:MM:SS)
+  const formattedCount = String(count).padStart(6, '0');
+
+  return (
+    <div className="mainContainer d-flex justify-content-center">
+      <div className="clock icon">0</div>
+      {formattedCount.split('').map((digit, index) => (
+        <div key={index} className={`clock number${index}`}>
+          {digit}
         </div>
-    );
-};
+      ))}
+      <button onClick={handleStart}>{isRunning ? 'Pause' : 'Start'}</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+}
 
-export default Clock
+export default Clock;
